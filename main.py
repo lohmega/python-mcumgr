7e324ac191bc6d0015f72484535947fc4915fcbd
Simon Frank <521620+5frank@users.noreply.github.com>
2020-11-24 00:07:26 +0100

    added cbor

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import logging
 import ble
 import smp
 import sys
-
+import cbor
 
 def cancel_tasks():
     # Cancel all task to ensure all connections closed.  Otherwise devices
@@ -20,15 +20,18 @@ def signal_handler(signo):
 
 
 async def run():
+    # TODO abstract enocde/decode!?
     req = smp.MgmtMsg()
     req.hdr.nh_op = smp.MGMT_OP.READ
     req.hdr.nh_id = smp.Mynewt.OS_MGMT_ID.ECHO
-    req.set_payload("hello2")
+    data = cbor.dumps({"d": "hello" })
+    req.set_payload(data)
     async with ble.SMPClientBLE(name="hwt_lmin-0000") as clnt:
         await clnt.write_msg(req)
         rsp = await clnt.read_msg()
     print(vars(rsp.hdr))
     print(rsp.payload.hex())
+    print(cbor.loads(rsp.payload))
 
 
 def set_verbose(verbose_level):
