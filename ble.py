c0246b6dd282e1b73c740f9c621080b6ebe7afb8
Simon Frank <521620+5frank@users.noreply.github.com>
2020-11-25 14:36:30 +0100

    pair before connect

--- ble.py
+++ ble.py
@@ -23,6 +23,7 @@ import time
 
 
 class _Queue:
+    """ Queue shared between asynchronous and synchronous code"""
     def __init__(self):
         self._loop = asyncio.get_running_loop()
         self._queue = asyncio.Queue()
@@ -39,7 +40,7 @@ class _Queue:
             self._queue.get(item), self._loop
         ).result()
 
-    def async_put_nowait(self, item):
+    def aput_nowait(self, item):
         self._queue.put_nowait(item)
 
     async def aput(self, item):
@@ -139,16 +140,12 @@ class SMPClientBLE:
         if address is None and name is None:
             raise ValueError("No device identifier. Need address or name")
 
-        self._read_cb = read_cb
-        self._read_evt = asyncio.Event()
-        self._read_buf = bytearray()
-        self._read_size = 0
-        self._read_msg_q = _Queue()
-
         self._address = address
         self._name = name
         self._timeout = timeout
-
+        self._read_cb = read_cb
+        self._read_buf = bytearray()
+        self._read_msg_q = _Queue()
         self._clnt = None
 
     def _set_disconnected_callback(self, cb):
@@ -169,14 +166,21 @@ class SMPClientBLE:
         dev = await find_device(self._address, self._name, self._timeout)
         if not dev:
             raise RuntimeError("Device not found")
+
         logger.debug("Device found %s", str(dev))
 
-        if platform.system() == "Linux":
-            bluetoothctl(dev)
-
-        await asyncio.sleep(1)
-
         self._clnt = BleakClient(dev, timeout=self._timeout)
+
+        try:
+            paired = self._clnt.pair()
+            if not paired:
+                logger.warning("not paired")
+        except NotImplementedError as e:
+            if platform.system() == "Darwin":
+                pass # pairing is automagic in MacOS
+            else:
+                raise e # probably old bleak version
+
         # self._set_disconnected_callback(self._on_disconnect)
         await self._clnt.connect(timeout=self._timeout)
         await self._clnt.start_notify(UUID_CHARACT, self._response_handler)
@@ -201,11 +205,13 @@ class SMPClientBLE:
             return
 
         logger.debug("received msg size %d", msg.size)
-        self._read_msg_q.put_nowait(msg)
-        self._read_buf = self._read_buf[msg.size :]  # keep what is not part of msg
+        # keep data that is not part of the msg
+        self._read_buf = self._read_buf[msg.size :]
 
-        if not self._read_evt.is_set():
-            self._read_evt.set()
+        if self._read_cb:
+            self._read_cb(self, msg)
+        else:
+            self._read_msg_q.put_nowait(msg)
 
     def _on_disconnect(self, client, _x=None):
         raise RuntimeError("Disconnected")
@@ -226,16 +232,8 @@ class SMPClientBLE:
         await self.write(msg.to_bytes())
 
     async def read_msg(self, timeout=None):
+        if self._read_cb:
+            raise RuntimeError("blocking read not allowed when callback set")
+
         return await self._read_msg_q.aget(timeout=timeout)
-
-    async def write_read(self, data, read_size=None, timeout=0):
-        self._read_evt.clear()
-        self._read_buf.clear()
-        self._read_size = read_size
-        self.write(data)
-
-        if timeout:
-            await asyncio.wait_for(self._read_evt.wait(), timeout)
-        else:
-            self._read_evt.wait()
 
